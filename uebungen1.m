9cfe6ec8ecb060df88376b4f8aec693056bf4f7a
Sidney <sidneyjohn23@icloud.com>
2019-03-11 21:25:16 +0100

    fixes

--- uebungen1.m
+++ uebungen1.m
@@ -1,16 +1,17 @@
+if (exist('OCTAVE_VERSION', 'builtin') ~= 0)
+	pkg load communication
+end
 fprintf("Aufgabe 1:\n")
-M = [1 2 3; 4 5 6; 7 8 9]
+M = vec2mat(1:9,3)
 k = 2.75
-w = [1 -1/2 1/3 -1/4;]
+w = -1.^(2:5).*(1:4).^-1
 y = 1.0:0.5:100.5
 fprintf("Aufgabe 2 a:\n")
-N = [M M; M M]
+N = repmat(M,2)
 fprintf("Aufgabe 2 b:\n")
-N23 = N;
-N23(2,:) = [];
-N23 (:,3) = []
+N23 = N([1 3:6],[1:2 4:6])
 fprintf("Aufgabe 2 c:\n")
-N(4,:) = [1 2 3 4 5 6]
+N(4,:) = 1:6
 fprintf("Aufgabe 2 d:\n")
 N(4,2) = 5 + i
 fprintf("Aufgabe 3:\n")
@@ -23,7 +24,7 @@ sum(x.*y)
 dot(x,y)
 fprintf("Aufgabe 4 b:\n")
 A = [ -1 2 3; 0 1 4 ];
-B = [ 9 8; 7 6; 5 4 ];
+B = vec2mat(9:-1:4,2);
 A*B
 fprintf("Aufgabe 5:\n")
 C = randi([-9 9], 4, 4)
@@ -33,25 +34,23 @@ C([4 1],:)
 C(:,4:-1:1)
 fprintf("Aufgabe 6:\n")
 K = [1 2; 4 3; 1 1; 4 0; 9 1]
-for i=1:size(K)(1);
-    K(i,:) = [sqrt(K(i,1)^2 + K(i,2)^2),atan(K(i,2)/K(i,1))]
-end
+[sqrt(K(:,1).^2 + K(:,2).^2),atan(K(:,2)./K(:,1))]
 fprintf("Aufgabe 7:\n")
-K1 = [1 2; 4 3; 1 1; 4 0; 9 1;];
-fliplr(cart2pol(K1(:,1),K1(:,2)))
+fliplr(cart2pol(K(:,1),K(:,2)))
 fprintf("Aufgabe 8:\n")
 t = 0:0.1:10
 clear e;
 s = sin(2*pi*t).*cos(3*pi*t)+e.^(-0.1*t)
 fprintf("Aufgabe 10:\n")
-x = [ 1 2 3 ];
-y = [ -1 0 1 ];
+x = 1:3;
+y = -1:1;
 a = x + y - 2
 b = x.^(-y)
 c = x./y
 try
 	d = x * y
 catch
+	fprintf("Aufgabe 10 d nicht lösbar.")
 end
 e1 = x.' * y
 f = x * y.'
